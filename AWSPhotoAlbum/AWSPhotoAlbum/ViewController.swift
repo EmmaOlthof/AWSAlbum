2d074e8add033e1fd648b833d903b926e0f6d75b
EmmaOlthof <emmaolthof@hotmail.com>
2021-01-14 10:24:18 +0100

    Initial Commit
    Add project, podfile and pods

---
+++ AWSPhotoAlbum/AWSPhotoAlbum/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AWSPhotoAlbum
+//
+//  Created by Rick Gijsberts on 14/01/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ AWSPhotoAlbum/Pods/Target Support Files/Pods-AWSPhotoAlbum/Pods-AWSPhotoAlbum-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_AWSPhotoAlbum : NSObject
+@end
+@implementation PodsDummy_Pods_AWSPhotoAlbum
+@end
